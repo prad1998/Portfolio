d8be3e6444d7a4ffa7b2502c6454c51b4562fef4
Pradeep Thayaparan <prtha18@student.sdu.dk>
2023-11-20 18:25:52 +0100

    init

---
+++ Portfolio/src/components/Main.svelte
@@ -0,0 +1,71 @@
+<script>
+
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+   <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+  <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+
+    <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+        Hi! I'm <span class="poppins text-violet-400">Pradeep</span> Thayaparan <br/> <span class="poppins text-violet-400">Developer</span>
+
+
+    </h2>
+    <p class="text-base sm:text-lg md:text-xl"></p>
+  </div>
+
+   <div class="relative shadow-2xl grid place-items-center">
+
+    <img src={"images/me.png"} alt="profile image" class="object-cover z-[2] max-h-[70vh]"/>
+    
+
+   </div>
+
+   </section>
+   
+   <section id=projects class="py-20 lg:py-32 flex flex-col gap-24">
+     <div class="flex flex-col gap-2 text-center">
+      <div class="border-t border-dotted border-purple-500 w-full my-8"></div>
+
+      <h6 class="text-large sm:text-xl md:text-2xl"> 
+        A few of my projects...
+      </h6>      
+
+      <div class="mx-auto w-16 h-2 bg-purple-500 my-8"></div>
+
+      
+      <h3 class="font-medium text-xl sm:text-2xl md:text-3xl"> Game User Interface </h3>
+
+      A Game User Interface that should Take In Camera Input and feed it into a Model-Free Reinforcement Learning method called Q-learning which then
+      should change the game parameters and thus give a good gaming experience. The technologies/Languages used in this project are <strong class=" text-violet-400">Javascript/Typescript, CSS, HTML, Node.js and React. </strong> 
+
+       
+      <div class="mx-auto w-16 h-2 bg-purple-500 my-8"></div>
+
+      
+
+       </div>
+
+   </section>
+
+
+
+   <section id=about class="py-20 lg:py-32 flex flex-col gap-24">
+    <div class="flex flex-col gap-2 text-center">
+     <h6 class="text-large sm:text-xl md:text-2xl"> 
+      <div class="border-t border-dotted border-purple-500 w-full my-8"></div>
+
+       About Me...
+
+       <div class="mx-auto w-16 h-2 bg-purple-500 my-8"></div>
+
+
+     </h6>
+     <h3>
+       cur
+     </h3>
+    </div>
+  </section>
+
+</main>
